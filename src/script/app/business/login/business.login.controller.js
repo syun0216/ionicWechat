b7fb3d994c1f615de5fb1183dc5674e07db496f8
G150SG <517897442@qq.com>
2016-09-07 22:57:34 +0800

    登录成功后记录username在localstorage

--- src/script/app/business/login/business.login.controller.js
+++ src/script/app/business/login/business.login.controller.js
@@ -9,7 +9,7 @@
         $scope.info = {
             phone:"",
             password:""
-        }
+        };
         $scope.username = 'june';
         var username = $cookieStore.get("username");
         if(username != null){
@@ -19,6 +19,7 @@
         $scope.confirmLogin = function () {
             if($scope.info.phone == 18022129789 && $scope.info.password == 1234){
                 $cookieStore.put('username',$scope.username);
+                localStorage.username = $scope.username;
                 $state.go('BusinessMainInterface',{name:$scope.username});
             }
             else{
--- src/script/app/business/mainInterface/business.mainInterface.controller.js
+++ src/script/app/business/mainInterface/business.mainInterface.controller.js
@@ -6,7 +6,7 @@
     angular.module("app.business")
         .controller("BusinessMainInterfaceCtrl",['$scope','$stateParams','$cookieStore','$state',BusinessMainInterfaceCtrl]);
     function BusinessMainInterfaceCtrl($scope,$stateParams,$cookieStore,$state){
-        $scope.myName = $stateParams.name;
+        $scope.myName = $stateParams.name == null ? $cookieStore.get('username') : $stateParams.name;
         if($scope.myName == null){
             $scope.myName = $cookieStore.get('username');
         }
